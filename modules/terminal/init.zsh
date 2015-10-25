a96318edd76865ed60e991358fbdac234b3982d6
Minijackson <minijackson@riseup.net>
2015-10-26 00:35:29 +0100

    Setting window title to hostname: path + cmd name shell expansion

--- modules/terminal/init.zsh
+++ modules/terminal/init.zsh
@@ -56,7 +56,7 @@ function _terminal-set-titles-with-command {
     )
   else
     # Set the command name, or in the case of sudo or ssh, the next command.
-    local cmd="${${2[(wr)^(*=*|sudo|ssh|-*)]}:t}"
+	local cmd="${(e)${2[(wr)^(*=*|sudo|ssh|-*)]}:t}"
     local truncated_cmd="${cmd/(#m)?(#c15,)/${MATCH[1,12]}...}"
     unset MATCH
 
